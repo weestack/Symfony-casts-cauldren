f483040a171a97773f7881186c5fd0083a924963
Weestack <Alexander@helius.dk>
2021-04-19 23:02:23 +0200

    made my first routes YAY

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuestionController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage() {
+        return new Response("<h1>hello world</h1>", 400);
+    }
+
+    /**
+     * @Route("/questions/{slug}/")
+     */
+    public function show($slug) {
+
+        return new Response(sprintf(
+            "Hello %s", ucwords(str_replace("-", " ", $slug))
+        ), 200);
+    }
+
+
+}
